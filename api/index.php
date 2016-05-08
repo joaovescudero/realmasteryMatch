912a0c0f91156980caed57b0f5d1ccf8a045267f
Joao Vitor Sousa Escudero <joaovescudero@gmail.com>
2016-05-08 16:22:16 -0300

    Testing CROS

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
   //
-  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Origin: http://localhost/");
 
   //Including config file
   require_once("config/config.php");
